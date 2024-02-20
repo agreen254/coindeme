1c406ea5c405b6c9127ab7a3f8c5ebe6e3135542
Aaron Green <aarongreen1997@gmail.com>
2024-02-20 01:02:31 -0600

    add custom caret icon

---
+++ Icons/Caret.tsx
@@ -0,0 +1,9 @@
+import { SVGProps } from "react";
+
+const CaretIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 4" {...props}>
+    <path d="M4 .333.668 3.666h6.667L4.001.333Z" />
+  </svg>
+);
+
+export default CaretIcon;
