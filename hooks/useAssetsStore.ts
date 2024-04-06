8e536f90af8b83c28803d4b5ce72387415e56836
agreen254 <aarongreen1997@gmail.com>
2024-04-05 23:23:32 -0500

    revert type

--- hooks/useAssetsStore.ts
+++ hooks/useAssetsStore.ts
@@ -1,15 +1,7 @@
 import type { AddedAsset } from "@/utils/types";
 
 import { addedAssetSchema } from "@/validation/schema";
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
 import toast from "react-hot-toast";
-
-type AssetStore = {
-  
-}
-
-export const createAssetStore = create<>()(persist((set) => ({})));
 
 export function validateAsset(asset: AddedAsset) {
   const validation = addedAssetSchema.safeParse(asset);
