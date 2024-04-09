d5029bde76e4606db7d6f8fea9247c71b942818b
agreen254 <aarongreen1997@gmail.com>
2024-04-08 22:51:32 -0500

    animate assets on entry

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { useAssetStore } from "@/hooks/useAssetStore";
 import { useAssetQueries } from "@/hooks/useAssetQueries";
+import { useMarketQuery } from "@/hooks/useMarketQuery";
 
+import { AnimatePresence } from "framer-motion";
 import AssetModalWrapper from "./AssetModal/AssetModalWrapper";
 import AssetDisplay from "./AssetDisplay";
 
 const Portfolio = () => {
   const assets = useAssetStore().assets;
-  const response = useAssetQueries(assets);
+  const assetResponse = useAssetQueries(assets);
+  // const marketResponse = useMarketQuery("usd", "market_cap", "desc");
+  // const currentAssetData = assets.map(asset => marketResponse?.data?.pages.)
 
   return (
     <div className="flex flex-col items-center">
@@ -20,12 +25,17 @@ const Portfolio = () => {
       </div>
       {/* <p className="italic mt-12 text-muted-foreground">No assets found.</p> */}
       <div className="flex flex-col gap-y-10 mt-10 mb-[50vh]">
-        {response.map((res, idx) => (
-          <AssetDisplay
-            key={assets[idx].assetId}
-            asset={assets[idx]}
-            response={res}
-          />
+        {assetResponse.map((res, idx) => (
+          <AnimatePresence key={assets[idx].assetId}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: 0.02 * idx }}
+            >
+              <AssetDisplay asset={assets[idx]} response={res} />
+            </motion.div>
+          </AnimatePresence>
         ))}
       </div>
     </div>
