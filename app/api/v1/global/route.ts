24e2355a94fad3f701abb7b70938ea0e087b6147
agreen254 <aarongreen1997@gmail.com>
2024-04-12 12:46:54 -0500

    add comment

--- app/api/v1/global/route.ts
+++ app/api/v1/global/route.ts
@@ -7,6 +7,12 @@ import { unstable_noStore as noStore } from "next/cache";
 
 // eslint-disable-next-line
 export async function GET(req: NextRequest) {
+  /**
+   * This route will be compiled as a static page at build-time and return a 304 Not Modified status whenever the page is loaded.
+   * This is bad because the user will not get updated data.
+   * 
+   * NoStore() method will force it to be a dynamic page and refetch from the coingecko api each time.
+   */
   noStore();
   const urlExtractor = () => "https://api.coingecko.com/api/v3/global";
 
