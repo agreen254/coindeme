631bdd0903867f1f47e0664e2518d8c8844a4577
Aaron Green <aarongreen1997@gmail.com>
2024-04-18 14:04:10 -0500

    update routes to use common behaviors

--- app/api/v1/comparison/route.ts
+++ app/api/v1/comparison/route.ts
@@ -7,18 +7,20 @@ import {
   comparisonChartRequestSchema,
   comparisonChartResponseSchema,
 } from "@/validation/schema";
+import { composeFetchUrl } from "@/utils/composeFetchUrl";
 import { postValidationHandler } from "@/validation/handler";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const urlExtractor = (body: ComparisonChartRequest) => {
+  function urlExtractor(body: ComparisonChartRequest) {
     const { id, currency, days } = body;
 
-    const fetchHead = "https://api.coingecko.com/api/v3/coins";
-    const fetchBody = `/${id}/market_chart?vs_currency=${currency}&days=${days}`;
-    const fetchTail = `&x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`;
-    return [fetchHead, fetchBody, fetchTail].join("");
-  };
+    return composeFetchUrl({
+      head: "   https://api.coingecko.com/api/v3/coins                     ",
+      body: `   /${id}/market_chart?vs_currency=${currency}&days=${days}   `,
+      tail: `   &x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}        `,
+    });
+  }
 
   return postValidationHandler<ComparisonChartRequest, ComparisonChartResponse>(
     req,
--- app/api/v1/table/route.ts
+++ app/api/v1/table/route.ts
@@ -1,18 +1,20 @@
 import type { Market, MarketRequest, MarketResponse } from "@/utils/types";
 
+import { composeFetchUrl } from "@/utils/composeFetchUrl";
 import { marketRequest, marketSchema } from "@/validation/schema";
 import { NextRequest } from "next/server";
 import { postValidationHandler } from "@/validation/handler";
 
 export async function POST(req: NextRequest) {
-  const urlExtractor = (body: MarketRequest) => {
+  function urlExtractor(body: MarketRequest) {
     const { page, currency, fetchParam, fetchOrder } = body;
 
-    const fetchHead = "https://api.coingecko.com/api/v3/coins/markets";
-    const fetchBody = `?vs_currency=${currency}&order=${fetchParam}_${fetchOrder}&per_page=50&page=${page}&sparkline=true&price_change_percentage=1h%2c24h%2c7d`;
-    const fetchTail = `&x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}`;
-    return [fetchHead, fetchBody, fetchTail].join("");
-  };
+    return composeFetchUrl({
+      head: "   https://api.coingecko.com/api/v3/coins/markets                                                                                             ",
+      body: `   ?vs_currency=${currency}&order=${fetchParam}_${fetchOrder}&per_page=50&page=${page}&sparkline=true&price_change_percentage=1h%2c24h%2c7d   `,
+      tail: `   &x_cg_demo_api_key=${process.env.COINGECKO_API_KEY}                                                                                        `,
+    });
+  }
 
   // make sure the information for the next page is contained in the response
   const responseTransfomer = (
