51ac954bda531078189dc956df2f62042b9e6404
agreen254 <aarongreen1997@gmail.com>
2024-06-04 01:43:21 -0500

    fix field

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { useMarketParams } from "@/hooks/useMarketParams";
 import { useMarketQuery } from "@/hooks/useMarketQuery";
+import { useSearchParams } from "next/navigation";
 import { useUserCurrencySetting } from "@/hooks/useUserSettings";
 
 import CarouselWrapper from "@/components/Carousel/CarouselWrapper";
 import ComparisonChartsWrapper from "@/components/ComparisonCharts/ComparisonChartsWrapper";
 import MarketTableMainWrapper from "@/components/MarketTable/MarketTableMainWrapper";
+import { DEFAULT_MARKET_FIELD } from "@/validation/defaults";
+import { MarketFetchField } from "@/utils/types";
 
 export default function Home() {
   const currency = useUserCurrencySetting();
-  const { field, order } = useMarketParams();
-  const queryResult = useMarketQuery(currency, field, "desc");
+  const searchParams = useSearchParams();
+  const field = (searchParams.get("field") ||
+    DEFAULT_MARKET_FIELD) as MarketFetchField;
 
+  const queryResult = useMarketQuery(currency, field, "desc");
   return (
     <main className="flex flex-col items-center gap-y-4">
       <CarouselWrapper axis="x" queryResult={queryResult} />
