70639e2e3f74acb255206c54c0407ba186cb777c
agreen254 <aarongreen1997@gmail.com>
2024-08-14 15:39:42 -0500

    remove example page

--- app/example/page.tsx
+++
@@ -1,9 +0,0 @@
-const ExamplePage = () => {
-  return (
-    <div className="w-full flex items-center justify-center">
-      <div className="w-[calc(100%-4rem)] bg-red-500 p-4 text-center rounded-lg">Example Page</div>
-    </div>
-  );
-};
-
-export default ExamplePage;
